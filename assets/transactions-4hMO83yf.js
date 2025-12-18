1ca59ebd59651e43121764b06692759f77111eab
memeswap888 <memeswaptop@outlook.com>
2025-12-18 16:18:35 +0800

    Add files via upload

---
+++ assets/transactions-4hMO83yf.js
@@ -0,0 +1,16 @@
+import{h as f,i as a,x as m}from"./index-dkYsPOcA.js";import{c as p}from"./index-CL1iFcyz.js";import"./index-BBNlPAAu.js";import"./index-DVZhmU3V.js";import"./if-defined-1Aki1NaQ.js";import"./index-CzlHrgxG.js";import"./index-DH4DqFdK.js";import"./index-BqAkfgxj.js";const d=f`
+  :host > wui-flex:first-child {
+    height: 500px;
+    overflow-y: auto;
+    overflow-x: hidden;
+    scrollbar-width: none;
+  }
+
+  :host > wui-flex:first-child::-webkit-scrollbar {
+    display: none;
+  }
+`;var u=function(o,t,i,r){var n=arguments.length,e=n<3?t:r===null?r=Object.getOwnPropertyDescriptor(t,i):r,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")e=Reflect.decorate(o,t,i,r);else for(var c=o.length-1;c>=0;c--)(l=o[c])&&(e=(n<3?l(e):n>3?l(t,i,e):l(t,i))||e);return n>3&&e&&Object.defineProperty(t,i,e),e};let s=class extends a{render(){return m`
+      <wui-flex flexDirection="column" .padding=${["0","3","3","3"]} gap="3">
+        <w3m-activity-list page="activity"></w3m-activity-list>
+      </wui-flex>
+    `}};s.styles=d;s=u([p("w3m-transactions-view")],s);export{s as W3mTransactionsView};
